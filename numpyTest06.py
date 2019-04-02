3afa06d4a0ea9be9f3b1d5914b6ed55ffcabf477
powerdev73 <46700646+powerdev73@users.noreply.github.com>
2019-04-02 17:56:32 +0900

    반갑습니다.

--- numpyTest06.py
+++ numpyTest06.py
@@ -1,18 +1,96 @@
 import numpy as np
 
-b =  np.arange(6).reshape(-1,3)
-# b =  np.arange(6).reshape(2,-1)
-# b =  np.arange(6).reshape(2,3)
+name = ['차범근','홍길동','강감찬','이순신','유관순','박주호','문재인']
+score = [100,80,90,100,70,95,100]
 
-print(b)
-print(b + 1)
-print(b>1)          # BroadCasting   b의 요소만큼 비교하여 True, False를 반환
-print(b[b>1])       # BoardCasting의 True요소만 출력
-# print(b[0][0])
+name = np.array(name)
+score = np.array(score)
+
+tmpidx = np.argwhere(score==np.amax(score))
+print(tmpidx.flatten())
+idx = tmpidx.flatten()
+
+print(name[idx])
 
 
-# a = np.arange(3)
+# name = ['홍길동','강감찬','이순신','유관순','박주호','김경민', '문재인']
+# score = [80,90,100,70,100,95,100]
+#
+# name = np.array(name)
+# score = np.array(score)
+# s = score.argsort()[::-1]
+#
+# print(name[s])
+# print(score[s])
+#
+# print(score.argmax())
+
+# a = [4,3,1,5,2]
+# arr1 = np.array(a)
+# print(arr1)
+#
+# arr2 = arr1.argsort()
+# print(arr2)
+#
+# print(arr1[arr2])
+
+# arr1.sort()
+# print(arr1)
+
+# a = np.zeros([5,5], dtype=np.int)
 # print(a)
-# print(a+1)          #broadCasting
-# print(a>1)          #broadCasting
-# print( a[a>1])
+#
+# a = np.eye(5,5, dtype=np.int)
+# print(a)
+#
+# a = [1,3,0,3,1]
+# b = np.eye(5,4, dtype=np.int)[a]
+# print(b)
+
+# a[range(5),range(5)]=1
+# print(a)
+
+# a = [1,3,0,3,1]
+#
+# arr1 = np.zeros([len(a),np.max(a)+1], dtype=np.int)
+# arr1[[range(len(a))],a]=1
+# print(arr1)
+#
+# print(type(range(5)))
+
+# a = np.arange(12).reshape(-1,4)
+# print(a)
+# print("="*20)
+#
+# print(a[[1,0],[0,1]])   #Index Array
+#
+# a[[1,0],[0,1]] = 100
+# print(a)
+#
+# a = np.ones([5,5], dtype=np.int)
+# b = np.zeros([5,5], dtype=np.int)
+#
+# a[1:-1,1:-1] = 0
+# print(a)
+#
+# b[:,[0,-1]]=1
+# print(b)
+# b[[0,-1],:]=1
+# print(b)
+
+# a = np.zeros([5,5], dtype=np.int)
+# print(a)
+#
+# a[[0,1,2,3,4],[0,1,2,3,4]]=1
+# print(a)
+
+# print(a[0,1])
+# print("="*20)
+# print(a[0])
+# print("="*20)
+# print(a[[0,1]])
+# print("="*20)
+# print(a[[1,0]])
+# print("="*20)
+# print(a>5)
+# print(a[a>5])
